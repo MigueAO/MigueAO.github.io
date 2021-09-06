132b9492580bea61b2435b526e567a78699c70a8
Miguel Alayo <mibe2904@gmail.com>
2021-09-05 20:56:54 -0500

    Botón Colorear

--- js/lab05.js
+++ js/lab05.js
@@ -36,7 +36,21 @@
             function obtenerNombreMes(dateString){
                 let fecha = new Date(dateString);
                 let mes = fecha.toLocaleString('es',{month: 'long'});
-                return mes;
+                return mes.charAt(0).toUpperCase() + mes.slice(1);;
+            }
+        }
+
+        //Botón para cambiar color de celdas
+        document.getElementById("btnColorear").onclick = function() {
+            let celdaAzul = document.getElementsByClassName("azul");
+            let celdaRoja = document.getElementsByClassName("rojo");
+
+            for(let azul=0; azul<=celdaAzul.length-1; azul++){
+                celdaAzul[azul].style.background = (celdaAzul[azul].style.background==="rgb(191, 71, 27)")? "#1F72A6":"#BF471B";
+            }
+
+            for(let rojo=0; rojo<=celdaRoja.length-1; rojo++){
+                celdaRoja[rojo].style.background = (celdaAzul[rojo].style.background==="rgb(31, 114, 166)")? "#BF471B":"#1F72A6";
             }
         }
     }
